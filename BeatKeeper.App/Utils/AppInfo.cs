117d082cb3dae11f1ccc51526dd563879118d4d3
Raphael Guntersweiler <raphael+git@gunti.cloud>
2021-08-17 23:48:51 +0200

    Added an "Informational Version"

--- BeatKeeper.App/Utils/AppInfo.cs
+++ BeatKeeper.App/Utils/AppInfo.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace BeatKeeper.App.Utils
 {
     public static class AppInfo
     {
         public static readonly string AppName = "Beat Saber Keeper - Reborn";
-        public static readonly Version AppVersion = Assembly.GetExecutingAssembly().GetName().Version;
+        public static readonly string AppVersion = Assembly.GetExecutingAssembly()
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
+            .InformationalVersion;
     }
 }
