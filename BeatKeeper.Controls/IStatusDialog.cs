ef58313da9c936d3ed43251d2f67a1532fabcfe1
Raphael Guntersweiler <raphael+git@gunti.cloud>
2021-08-20 23:22:30 +0200

    removed A LOT of old stuff

--- BeatKeeper.Controls/IStatusDialog.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BeatKeeper.Controls
-{
-    public interface IStatusDialog
-    {
-        void SetStatus(string status);
-        void SetStatus(string status, int value, int maxValue = 100);
-    }
-}
